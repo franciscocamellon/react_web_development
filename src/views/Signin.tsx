b2fb4ce505a6cda3d32a833ca4a4231a31aa249e
Francisco Alves Camello Neto <franciscocamellon@gmail.com>
2024-11-17 13:36:39 -0300

    chore: Tradução finalizada (#5)

--- src/views/Signin.tsx
+++ src/views/Signin.tsx
@@ -3,11 +3,15 @@ import { useAppContext } from "../Context";
 import { Button, DatePicker, DateTimePicker } from "../components";
 
 const SignIn: React.FC = () => {
-  const { showSnackMessage, showAlertMessage } = useAppContext();
+  const { showSnackMessage, showAlertMessage, translate, changeLanguage } = useAppContext();
+  console.log(translate);
 
   return (
     <>
-      <Button onClick={() => showSnackMessage("Aqui temos uma mensagem")}>Clique snack</Button>
+      <Button onClick={() => showSnackMessage("Aqui temos uma mensagem")}>{translate("welcome")}</Button>
+      <Button onClick={() => changeLanguage("es")}>ES</Button>
+      <Button onClick={() => changeLanguage("en")}>EN</Button>
+      <Button onClick={() => changeLanguage("pt")}>PT</Button>
 
       <Button onClick={() => showAlertMessage("Aqui temos uma mensagem", "success")}>Clique alert</Button>
 
